4ce182f3489906fd9c2b12181adcddefc25911ed
JJk <wnsrb1594@naver.com>
2024-09-04 11:27:19 +0900

    2_04

---
+++ src/main/java/com/example/sbb2/Answer.java
@@ -0,0 +1,24 @@
+package com.example.sbb2;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne //n:1관계
+    private Question question;
+}
