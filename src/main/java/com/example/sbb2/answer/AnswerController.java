f07f06a2c165438c15991d706c9b4391d0ac44f1
JJk <wnsrb1594@naver.com>
2024-09-09 23:01:10 +0900

    2-16 질문등록 추가하기

--- src/main/java/com/example/sbb2/answer/AnswerController.java
+++ src/main/java/com/example/sbb2/answer/AnswerController.java
@@ -2,12 +2,13 @@ package com.example.sbb2.answer;
 
 import com.example.sbb2.question.Question;
 import com.example.sbb2.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RequiredArgsConstructor
@@ -17,10 +18,15 @@ public class AnswerController {
     private final QuestionService questionService;
     private final AnswerService answerService;
     @PostMapping("/answer/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value = "content") String content){
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult){
         Question question = this.questionService.getQuestion(id);
 
-        this.answerService.create(question, content);
+        if(bindingResult.hasErrors()){
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+
+        this.answerService.create(question, answerForm.getContent());
 
         return String.format("redirect:/question/detail/%s", id);
 
