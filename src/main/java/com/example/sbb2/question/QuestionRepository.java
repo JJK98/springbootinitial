090678f88d15d025bb8547e928d62c00b567be9f
JJk <wnsrb1594@naver.com>
2024-09-20 12:19:59 +0900

    3-13 ê검색기능

--- src/main/java/com/example/sbb2/question/QuestionRepository.java
+++ src/main/java/com/example/sbb2/question/QuestionRepository.java
@@ -3,6 +3,7 @@ package com.example.sbb2.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +15,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
     Page<Question> findAll(Pageable pageable);
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
+
 }
